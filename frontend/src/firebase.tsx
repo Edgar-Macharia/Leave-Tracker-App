de88628f9522904a9a3d6ca674bb67e86cf15bac
Edgar Macharia <m.edgarmacharia@gmail.com>
2024-11-26 16:55:04 +0300

    fix

--- frontend/src/firebase.tsx
+++ frontend/src/firebase.tsx
@@ -1,13 +1,14 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
+import 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyD7k1yjOzIvbML3nAgLpdWbC9aiRyHpEiA",
+  apiKey: import.meta.env.FIREBASE_API_KEY,
   authDomain: "lta-file-storage.firebaseapp.com",
   projectId: "lta-file-storage",
   storageBucket: "lta-file-storage.appspot.com",
